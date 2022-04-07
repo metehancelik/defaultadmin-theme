f92b23e2bc810dc3bb9c32d22348fef8bb50b003
Metehan Celik <mthncelik@gmail.com>
2022-04-07 12:22:06 +0300

    admin panel translations

--- resources/js/theme/select2.js
+++ resources/js/theme/select2.js
@@ -4505,6 +4505,45 @@ S2.define('select2/i18n/en',[],function () {
     }
   };
 });
+S2.define('select2/i18n/tr',[],function () {
+  // Turkish
+    return {
+        errorLoading: function (){
+            return 'Sonuç yüklenemedi';
+        },
+        inputTooLong: function (args) {
+            var overChars = args.input.length - args.maximum;
+
+            var message = overChars + ' karakter daha girmelisiniz';
+
+            return message;
+        },
+        inputTooShort: function (args) {
+            var remainingChars = args.minimum - args.input.length;
+
+            var message = 'En az ' + remainingChars + ' karakter daha girmelisiniz';
+
+            return message;
+        },
+        loadingMore: function () {
+            return 'Daha fazla…';
+        },
+        maximumSelected: function (args) {
+            var message = 'Sadece ' + args.maximum + ' seçim yapabilirsiniz';
+
+            return message;
+        },
+        noResults: function () {
+            return 'Sonuç bulunamadı';
+        },
+        searching: function () {
+            return 'Aranıyor…';
+        },
+        removeAllItems: function () {
+            return 'Tüm öğeleri kaldır';
+        }
+    };
+});
 
 S2.define('select2/defaults',[
   'jquery',
